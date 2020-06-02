2275eb5107a35f396172f9393bf395cd5be4a043
Tanmoy Sarkar <sarkar741127@gmail.com>
2020-06-02 10:28:46 +0530

    Add Files via Upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request,jsonify,render_template
+from github import Github
+import os
+from dotenv import load_dotenv
+
+if os.path.exists('.env'):
+    load_dotenv()
+    token = os.getenv("token")
+    organization_name=os.getenv("organization_name")
+    redirection_url=os.getenv("redirection_url")
+
+app=Flask(__name__)
+@app.route('/')
+def index():
+    return render_template('register.html')
+@app.route('/invite',methods=['POST','GET'])
+def invite():
+    if(request.method=='GET'):
+        email=request.args.get('git_email')
+        gh = Github(token)
+        org = gh.get_organization(organization_name)
+
+        try:
+            org.invite_user(email=email)
+            return render_template('success.html',redirection_url=redirection_url)
+        except :
+            return render_template('unsuccess.html')
+
+if __name__== '__main__':
+    app.run(debug=True)
